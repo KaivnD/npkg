e49ab407d7d71b0137b8aeaa6ab7922cb9e287f7
KaivnD <KaivnD@hotmail.com>
2019-10-12 14:41:09 +0800

    添加项目文件。

---
+++ NPKG/NPKGInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+
+namespace NPKG
+{
+    public class NPKGInfo : GH_AssemblyInfo
+    {
+        public override string Name
+        {
+            get
+            {
+                return "NPKG";
+            }
+        }
+        public override Bitmap Icon
+        {
+            get
+            {
+                //Return a 24x24 pixel bitmap to represent this GHA library.
+                return null;
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                //Return a short string describing the purpose of this GHA library.
+                return "";
+            }
+        }
+        public override Guid Id
+        {
+            get
+            {
+                return new Guid("6484f739-2ea5-432f-9087-cf76a3a0450c");
+            }
+        }
+
+        public override string AuthorName
+        {
+            get
+            {
+                //Return a string identifying you or your company.
+                return "";
+            }
+        }
+        public override string AuthorContact
+        {
+            get
+            {
+                //Return a string representing your preferred contact details.
+                return "";
+            }
+        }
+    }
+}
