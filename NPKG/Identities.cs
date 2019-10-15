c2bbabf3ca82979cd931444f7d23b4bcfbcbe03d
KaivnD <KaivnD@hotmail.com>
2019-10-15 20:32:43 +0800

    opt:重整项目结构

--- NPKG/Identities.cs
+++ NPKG/Identities.cs
@@ -4,12 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NPKG.Components
+namespace NPKG
 {
     public static class Identities
     {
-        public static Guid Importer = new Guid("081372C7-3D43-49A7-8389-76FEFDA65D34");
-        public static Guid Exporter = new Guid("D8067053-BB9E-4C26-88A8-864338B5450F");
+        public static Guid InfoPanel = new Guid("089EDA49-33E5-4371-89C3-3FD5B1E6C2FA");
         public static Guid GBlock = new Guid("0D941CAA-4BC3-4057-A6EF-4C430D32B16E");
     }
 }
