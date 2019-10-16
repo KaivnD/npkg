6beae5e0e646ebd464679285d01f74e101da9b2c
KaivnD <KaivnD@hotmail.com>
2019-10-16 16:00:25 +0800

    add:Default component id

--- Components/Identities.cs
+++ Components/Identities.cs
@@ -10,5 +10,6 @@ namespace NPKG
     {
         public static Guid InfoPanel = new Guid("089EDA49-33E5-4371-89C3-3FD5B1E6C2FA");
         public static Guid GBlock = new Guid("0D941CAA-4BC3-4057-A6EF-4C430D32B16E");
+        public static Guid SetDefault = new Guid("996c325a-dc25-498f-b30f-257b938600a3");
     }
 }
