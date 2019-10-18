dc50caeb4aaebbd2f226834c449e870945e82acc
KaivnD <KaivnD@hotmail.com>
2019-10-19 00:55:13 +0800

    opt:挪窝

---
+++ GH_Plugin/Commons/PackageInfo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPKG
+{
+    public class PackageInfo
+    {
+        public PackageInfo()
+        {
+        }
+
+        public string name { set; get; }
+
+        public string version { set; get; }
+
+        public string description { set; get; }
+
+        public List<string> modules { set; get; }
+
+        public List<string> files { set; get; }
+
+        public string author { set; get; }
+
+        public string repository { get; set; }
+
+        public List<Dependency> dependencies { get; set; }
+    }
+
+    public class Dependency
+    {
+        public Dependency()
+        {
+        }
+
+        public string name { get; set; }
+
+        public string version { get; set; }
+    }
+}
